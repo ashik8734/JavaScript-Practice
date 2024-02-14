af03e1ee37463bbb711f299aa053544f71838f05
Ashikali Shaikh <121289788+ashik8734@users.noreply.github.com>
2024-02-14 13:03:18 +0530

    Add files via upload

---
+++ JavaScript/second.js
@@ -0,0 +1,82 @@
+/*Operators and Conditional Statements */
+
+//  1. Arthematic Operator
+// let a=5, b=4;
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+// console.log(a**b);
+// console.log(a--);
+// console.log(a++);
+
+// 2. Assignment Operator
+
+// let x=3, y=4;
+
+// console.log(x+=y);
+// console.log(x-=y);
+// console.log(x*=y);
+// console.log(x/=y);
+// console.log(x%=y);
+// console.log(x**=y);
+
+// 3.Comarisons Statements
+
+// a) if-else Statement: 
+// let age = Number.parseInt(prompt("Please enter something:"));
+
+// if(age>18){
+//     alert("You are Adult");
+// }
+// else if(age<5){
+//     alert("You are a Baby");
+// }
+// else{
+//     alert("You are a boy");
+// }
+
+// b) Switch Statement:
+
+// let day=Number.parseInt(prompt());
+
+// switch(day){
+//     case 0:
+//     alert("Sunday");
+//     break;
+
+//     case 1:
+//     alert("Monday");
+//     break;
+
+//     case 2:
+//     alert("Tuesday");
+//     break;
+
+//     case 3:
+//     alert("wednesday");
+//     break;
+
+//     case 4:
+//     alert("Thursday");
+//     break;
+
+//     case 5:
+//     alert("Friday");
+//     break;
+    
+//     case 6:
+//     alert("Saturday");
+//     break;  
+    
+//     default:
+//     alert("Invalid Input"); 
+//     break; 
+// }
+
+
+
+
+
